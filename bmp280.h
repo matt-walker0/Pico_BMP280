6a5caf2099f0c86bc2598e8394dfa493c476d25d
matt-walker0 <78490185+matt-walker0@users.noreply.github.com>
2022-04-09 21:03:20 +0100

    Rewrite based upon official RasPi example

--- bmp280.h
+++ bmp280.h
@@ -2,15 +2,10 @@
 #define bmp_included
 
 
-// Setup BMP280, returns HIGH flag if this process fails.
 bool BMP_Setup(i2c_inst_t* _i2c_port_, uint8_t _device_address_);
 
-
-// Reads ADC values from BMP280, converts these to temperature and pressure.
-// Returns HIGH flag if error.
-// Pressure given in Pa
-// Temperature in (1/100) of 1°C
 bool BMP_Values(uint32_t *pressure, int16_t *temperature);
 
+void BMP280_Reset();
 
 #endif
